4adf0d0b2439c6464ee531df00b957f26d46881c
Alex Reyes <akosialexpo@gmail.com>
2020-07-04 19:17:19 +0800

    fix: Redo Firebase implementation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,44 @@
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from 'prop-types'
+import React from 'react'
 
 const Contact = ({ siteTitle }) => (
-  <div className="contact">
-    <a className="contact__link" href="http://github.com/alexako/" target="_blank">
-      <i className="fab fa-github-alt fa-3x" aria-hidden="true"></i>
-    </a>
-    <a className="contact__link" href="https://twitter.com/kludgebot/" target="_blank">
-      <i className="fab fa-twitter fa-3x" aria-hidden="true"></i>
-    </a>
-    <a className="contact__link" href="https://linkedin.com/in/alexjreyes" target="_blank">
-      <i className="fab fa-linkedin-in fa-3x" aria-hidden="true"></i>
-    </a>
-    <a className="contact__link" href="mailto:me@alexjreyes.com?subject=Hi, Alex!">
-      <i className="far fa-envelope fa-3x" aria-hidden="true"></i>
-    </a>
-  </div>
+    <div className="contact">
+        <a
+            className="contact__link"
+            href="http://github.com/alexako/"
+            target="_blank"
+        >
+            <i className="fab fa-github-alt fa-3x" aria-hidden="true"></i>
+        </a>
+        <a
+            className="contact__link"
+            href="https://twitter.com/kludgebot/"
+            target="_blank"
+        >
+            <i className="fab fa-twitter fa-3x" aria-hidden="true"></i>
+        </a>
+        <a
+            className="contact__link"
+            href="https://linkedin.com/in/alexjreyes"
+            target="_blank"
+        >
+            <i className="fab fa-linkedin-in fa-3x" aria-hidden="true"></i>
+        </a>
+        <a
+            className="contact__link"
+            href="mailto:me@alexjreyes.com?subject=Hi, Alex!"
+        >
+            <i className="far fa-envelope fa-3x" aria-hidden="true"></i>
+        </a>
+    </div>
 )
 
 Contact.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Contact.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Contact
