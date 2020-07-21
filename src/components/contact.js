d8513e224963fd7bff37d3d3d07632bdda3e58f0
Alex Reyes <akosialexpo@gmail.com>
2020-07-22 01:21:06 +0800

    replace: Twitter with resume download and add tooltips

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { FaGithubAlt, FaTwitter, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
+import { FaGithubAlt, FaLinkedinIn, FaEnvelope, FaFileDownload } from 'react-icons/fa';
+import ReactTooltip from 'react-tooltip';
 
 const Contact = ({ siteTitle }) => (
     <div className="contact">
@@ -9,31 +10,39 @@ const Contact = ({ siteTitle }) => (
             rel="noreferrer"
             href="http://github.com/alexako/"
             target="_blank"
+            data-tip="Github"
         >
             <FaGithubAlt />
         </a>
+        <ReactTooltip effect='solid' place={'bottom'} />
         <a
             className="contact__link"
             rel="noreferrer"
-            href="https://twitter.com/kludgebot/"
+            href="https://firebasestorage.googleapis.com/v0/b/alexjreyes-d592a.appspot.com/o/Alexander_Joseph_Reyes__Web_Developer.docx?alt=media&token=eaeec4d7-6b44-4da3-acb0-be5f81061c9d"
             target="_blank"
+            data-tip="Download Resume"
         >
-            <FaTwitter />
+            <FaFileDownload />
         </a>
+        <ReactTooltip effect='solid' place={'bottom'} />
         <a
             className="contact__link"
             rel="noreferrer"
             href="https://linkedin.com/in/alexjreyes"
             target="_blank"
+            data-tip="LinkedIn"
         >
             <FaLinkedinIn />
         </a>
+        <ReactTooltip effect='solid' place={'bottom'} />
         <a
             className="contact__link"
             href="mailto:me@alexjreyes.com?subject=Hi, Alex!"
+            data-tip="Email me@alexjreyes.com"
         >
             <FaEnvelope />
         </a>
+        <ReactTooltip effect='solid' place={'bottom'} />
     </div>
 )
 
