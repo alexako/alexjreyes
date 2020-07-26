9afc81c2866f1528f92cd1632702915bd6a26aee
Alex Reyes <akosialexpo@gmail.com>
2020-07-27 01:25:10 +0800

    add: Implement navbar component in portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from '../components/seo'
 
 import Intro from '../components/intro'
 import Portfolio from '../components/portfolio/portfolio'
+import NavBar from '../components/navbar'
 
 const IndexPage = () => { 
     return (
