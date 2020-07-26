71cb945dffece9949b3c19b1e8a9f108d164398c
Alex Reyes <akosialexpo@gmail.com>
2020-07-26 23:36:50 +0800

    cleanup: Remove unused code and page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 import Intro from '../components/intro'
-import { graphql } from "gatsby"
 import Portfolio from '../components/portfolio/portfolio'
 import TagsList from '../components/tags'
 
 const IndexPage = ({data}) => { 
-  const projects = data.allMarkdownRemark.edges;
     return (
         <Layout>
             <SEO
@@ -30,38 +28,10 @@ const IndexPage = ({data}) => {
             />
 
             <Intro />
-            <Portfolio props={projects}/>
+            <Portfolio />
             <TagsList />
         </Layout>
     )
 }
 
 export default IndexPage
-
-export const query = graphql`
-  {
-    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          excerpt(pruneLength: 250)
-          frontmatter {
-            linkProject
-            linkCode
-            tags
-            date(formatString: "MMMM Do YYYY")
-            title
-            backgroundImage {
-              publicURL
-            }
-            image {
-              publicURL
-            }
-          }
-        }
-      }
-    }
-  }
-`
