991555948d356e760e68f55a3162e091725e02b7
Alex Reyes <akosialexpo@gmail.com>
2020-07-26 23:35:21 +0800

    add: Tag page builder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,32 @@
 const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+const Tags = {
+    JAVASCRIPT: { display: 'javascript', styles: { backgroundColor: '#F0DB4F' }},
+    TYPESCRIPT: { display: 'typescript', styles: { backgroundColor: '#2775C4' }},
+    ANGULAR: { display: 'angular', styles: { backgroundColor: '#FF3E3E' }},
+    REACT: { display: 'react', styles: { backgroundColor: '#5AD9F9' }},
+    REGEX: { display: 'regex', styles: { }},
+    GATSBY: { display: 'gatsby', styles: { backgroundColor: '#663399' }},
+    GRAPHQL: { display: 'graphql', styles: { backgroundColor: '#E34297' }},
+    COMPSCI: { display: 'compsci', styles: { }},
+    HEROKU: { display: 'heroku', styles: { backgroundColor: '#9E7CC1' }},
+    EXPRESSJS: { display: 'expressjs', styles: { backgroundColor: '#689F62' }},
+    PYTHON: { display: 'python', styles: { backgroundColor: '#417FB0' }},
+    MACHINELEARNING: { display: 'machine-learning', styles: { }},
+    DATAMODELING: { display: 'data-modeling', styles: { }},
+    KERAS: { display: 'keras', styles: { backgroundColor: '#FF3E3E' }},
+    FIREBASE: { display: 'firebase', styles: { backgroundColor: '#F9CB47' }},
+    UI: { display: 'ui', styles: { }},
+    SASS: { display: 'sass', styles: { backgroundColor: '#cc6599' }},
+}
+
 exports.createPages = async ({ actions, graphql }) => {
     const { createPage } = actions
 
     const PortfolioProjectTemplate = path.resolve(`src/templates/project-template.js`)
 
-    const result = await graphql(`
+    const projects = await graphql(`
     {
       allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
@@ -27,19 +47,29 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `)
 
-  if (result.errors) {
+  if (projects.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  projects.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: `/projects${node.fields.slug}`,
       component: PortfolioProjectTemplate,
       context: {
         slug: node.fields.slug,
         tags: node.frontmatter.tags
-      }, // additional data can be passed via context
+      },
+    })
+  })
+
+  Object.keys(Tags).forEach(tag => {
+    createPage({
+      path: `/projects/${Tags[tag].display}`,
+      component: path.resolve('src/components/portfolio/portfolio.js'),
+      context: {
+        tag: Tags[tag].display,
+      },
     })
   })
 }
